9ffe3092f68386552a733ae5fdd77ab17ac45bd9
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-09-19 10:04:55 +0700

    init cell class

---
+++ src/dev/wisesa/game_of_lyfe/Cell.java
@@ -0,0 +1,20 @@
+package dev.wisesa.game_of_lyfe;
+
+import java.awt.*;
+
+public class Cell extends Rectangle {
+    private boolean isAlive = false;
+
+    Cell(int x, int y, int width, int height, boolean state) {
+        super(x, y, width, height);
+        this.isAlive = state;
+    }
+
+    public boolean getState() {
+        return isAlive;
+    }
+
+    public void setState(boolean state) {
+        this.isAlive = state;
+    }
+}
